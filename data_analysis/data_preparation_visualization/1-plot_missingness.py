408469bd0de4cd6e409f78ed3a6a84858bf38c72
armanruet <armanruet@gmail.com>
2026-07-15 12:43:06 +0200

    Task 1 done

---
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_missingness(df):
+    """
+    """
+    plt.figure(figsize=(12, 8))
+
+    x_vals = []
+    y_vals = []
+    for i, col in enumerate(df.columns):
+        missing_idx = df.index[df[col].isnull()]
+        x_vals.extend(missing_idx)
+        y_vals.extend([i] * len(missing_idx))
+
+    plt.scatter(x_vals, y_vals, marker='|')
+    plt.yticks(range(len(df.columns)), df.columns)
+    plt.title("Missingness Plot")
+
+    plt.tight_layout()
+    plt.show()
