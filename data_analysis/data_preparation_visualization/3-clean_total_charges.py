a85589211ca6377d4c892f4f8d261bad0dc4c4ab
armanruet <armanruet@gmail.com>
2026-07-15 12:51:40 +0200

    Task 3

---
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Task 3"""
+import pandas as pd
+
+
+def clean_total_charges(df, method='drop'):
+    """Def the func"""
+    df = df.copy()
+
+    if method == 'drop':
+        df = df.dropna(subset=['TotalCharges'])
+    elif method == 'median':
+        median_val = df['TotalCharges'].median()
+        df['TotalCharges'] = df['TotalCharges'].fillna(median_val)
+    elif method == 'impute':
+        df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges'] * df['tenure'])
+
+    return df
