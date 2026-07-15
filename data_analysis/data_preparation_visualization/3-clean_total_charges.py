13e8934fa792fa1229821843fd458cf26aacb952
armanruet <armanruet@gmail.com>
2026-07-15 12:53:42 +0200

    Task 3

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -12,6 +12,7 @@ def clean_total_charges(df, method='drop'):
         median_val = df['TotalCharges'].median()
         df['TotalCharges'] = df['TotalCharges'].fillna(median_val)
     elif method == 'impute':
-        df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges'] * df['tenure'])
+        df['TotalCharges'] = df['TotalCharges'].fillna(
+            df['MonthlyCharges'] * df['tenure'])
 
     return df
