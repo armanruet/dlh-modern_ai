91f6c072ff0cdb4c2697bc9228d786969590bce1
armanruet <armanruet@gmail.com>
2026-07-15 12:49:09 +0200

    Task 2 done

---
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 2"""
+import pandas as pd
+
+
+def convert_columns(df):
+    """Def the func"""
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df['SeniorCitizen'] = df['SeniorCitizen'].map({0: 'No', 1: 'Yes'})
+    return df
