b375f82a5d9501e9bf8e81447943d223328b5c2a
armanruet <armanruet@gmail.com>
2026-07-22 16:59:52 +0200

    Tree model: Task 5 done

---
+++ machine_learning/tree_models/5-pre_pruning.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Write a function that uses Scikit-learn to perform a Grid Search
+for the best pre-pruning hyperparameters for a decision tree classifier.
+    """
+
+
+from sklearn import model_selection
+
+
+def prepruning(X, y, clf):
+    """   The search explores the following hyperparameters:
+    A dictionary containing the best combination
+    of hyperparameters found during the grid search.
+
+    """
+    param_grid = {
+        'criterion': ['gini', 'entropy'],
+        'max_depth': list(range(2, 5)),
+        'min_samples_leaf': list(range(2, 5)),
+        'min_samples_split': list(range(2, 5))
+    }
+    # cross-validation: split data into k, train on k-1, test on 1
+    grid_search = model_selection.GridSearchCV(
+        estimator=clf,
+        param_grid=param_grid,
+        cv=5,
+        scoring='accuracy'
+    )
+    grid_search.fit(X, y)
+    return grid_search.best_params_
