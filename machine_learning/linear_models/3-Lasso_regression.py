51d8af11dd4a47838deac62faaf6391c70328b1b
armanruet <armanruet@gmail.com>
2026-07-26 02:45:18 +0200

    Linear Model: task 3

---
+++ machine_learning/linear_models/3-Lasso_regression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module containing a function to instantiate a Lasso regression model. """
+
+from sklearn import linear_model
+
+
+def lasso_regression(random_state):
+    """ Creates and returns an untrained Lasso Regression model.
+
+    """
+    return linear_model.Lasso(random_state=random_state)
