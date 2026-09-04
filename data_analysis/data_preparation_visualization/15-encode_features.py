fcaf1318a7a3a4c741dc8685042ab7b8de9eeb9d
armanruet <armanruet@gmail.com>
2026-09-04 22:42:31 +0200

    data prep task 15 corrected

--- data_analysis/data_preparation_visualization/15-encode_features.py
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -8,6 +8,17 @@ from sklearn import preprocessing
 
 def encode_features(df):
     """df: pandas DataFrame
+The function should encode:
+    Churn: LabelEncoder (No→0, Yes→1)
+    Partner, Dependents, PaperlessBilling, SeniorCitizen:
+    OrdinalEncoder (No→0, Yes→1)
+    Contract, PaymentMethod: One-hot encoding with drop
+    first set to True
+    TenureGroup: Alphabetical order OrdinalEncoder
+Returns:
+    The encoded DataFrame
+    The Fitted LabelEncoder for Churn
+    The Fitted OrdinalEncoder for binary columns
     The Fitted OrdinalEncoder for TenureGroup
 
     Args:
