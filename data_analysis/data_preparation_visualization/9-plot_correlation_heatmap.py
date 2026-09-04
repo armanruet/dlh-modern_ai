2fb35981bdd542d80a1af2f9d114f37088aeab6a
armanruet <armanruet@gmail.com>
2026-09-04 11:46:28 +0200

    data prep task 9 done

---
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module for plotting correlation heatmap """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """ Plot an annotated correlation heatmap for numeric columns.
+    Returns:
+        None
+    """
+    plt.figure(figsize=(6, 5))
+
+    ndf = df.select_dtypes(include=['float64', 'int64'])
+    corr_matrix = ndf.corr()
+
+    sns.heatmap(
+        corr_matrix,
+        annot=True,
+        cmap='coolwarm',
+        vmin=-1,
+        vmax=1
+        )
+
+    plt.title("Correlation Matrix")
+    plt.show()
