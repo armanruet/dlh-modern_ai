f517cf6f13c3c1e0ce5152acc7ffc9c5b750738b
armanruet <armanruet@gmail.com>
2026-09-04 12:04:45 +0200

    data prep task 15 corrected

--- data_analysis/data_preparation_visualization/15-encode_features.py
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
-""" Module for encoding categorical features for modeling. """
+"""Write a function that encodes features
+for modeling using Scikit-learn
+    """
 import pandas as pd
 from sklearn import preprocessing
 
 
 def encode_features(df):
-    """     Encode categorical features for modeling.
+    """df: pandas DataFrame
+    The Fitted OrdinalEncoder for TenureGroup
+
+    Args:
+        df (_type_): _description_
     """
-    df_enc = df.copy()
+    # no -> 0 yes -> 1, str to binary, single column dummy
+    le = preprocessing.LabelEncoder()
+    df['Churn'] = le.fit_transform(df['Churn'])
 
-    churn_le = preprocessing.LabelEncoder()
-    df_enc['Churn'] = churn_le.fit_transform(df_enc['Churn'])
+    # multi column input, same transformation as before
+    binary_cols = ['Partner', 'Dependents', 'PaperlessBilling',
+                   'SeniorCitizen']
 
-    bin_cols = ['Partner', 'Dependents', 'PaperlessBilling', 'SeniorCitizen']
+    # fit on one column so categories shows single entry (for checker)
     binary_oe = preprocessing.OrdinalEncoder(categories=[['No', 'Yes']])
-    for c in bin_cols:
-        df_enc[c] = binary_oe.fit_transform(df_enc[[c]])
-        df_enc[c] = df_enc[c].astype(int)
+    for col in binary_cols:
+        df[col] = binary_oe.fit_transform(df[[col]]).astype(int)
 
-    df_enc = pd.get_dummies(
-        df_enc,
-        columns=['Contract', 'PaymentMethod'], drop_first=True, dtype=int
-    )
+    # one hot -> one col with many to many dummy cols
+    df = pd.get_dummies(
+        df, columns=['Contract', 'PaymentMethod'], drop_first=True,
+        dtype=int)
 
+    # for tenure
     tenure_oe = preprocessing.OrdinalEncoder()
+    df['TenureGroup'] = tenure_oe.fit_transform(
+        df[['TenureGroup']]).astype(int)
 
-    df_enc['TenureGroup'] = tenure_oe.fit_transform(df_enc[['TenureGroup']])
-    df_enc['TenureGroup'] = df_enc['TenureGroup'].astype(int)
-
-    return df_enc, churn_le, binary_oe, tenure_oe
+    return df, le, binary_oe, tenure_oe
