0c6553a60a4044e5208c0a0d745f2f7954786a71
armanruet <armanruet@gmail.com>
2026-09-05 16:17:15 +0200

    data prep task 13

---
+++ data_analysis/data_preparation_visualization/13-ttest_numeric.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Write a function that performs Welch's t-tests
+for continuous numeric features using scipy
+"""
+
+from scipy import stats
+
+
+def ttest_numeric(df):
+    """df: pandas DataFrame with Churn column
+
+Computes t-test p-value comparing Churn=Yes vs Churn=No
+for each numeric feature
+
+The Hypothesis being tested is:
+
+H₀ (null): The means of the variable are equal
+in Churn=Yes and Churn=No groups
+H₁ (alternative): The means differ significantly
+
+Returns a dictionary: {feature_name: p_value}
+
+    Args:
+        df (_type_): _description_
+    """
+    out = {}
+    num_cols = df.select_dtypes(include="number").columns  # keep numeric
+    for col in num_cols:
+        yes = df[df['Churn'] == 'Yes'][col]  # keep yes rows
+        no = df[df['Churn'] == 'No'][col]  # keep no rows
+        _, p = stats.ttest_ind(yes, no,  equal_var=False)  # Welch _var
+        out[col] = p  # store in dict
+    return out
